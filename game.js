80af904a476c40ff849e9b2273cc04f770aa96b2
Gombb <primegombb@protonmail.com>
2020-11-23 21:23:13 +0100

    feat: created slot prototype with minesweeper images

--- game.js
+++ game.js
@@ -1,7 +1,20 @@
 const game = {
+    const: slotImg = ["apple", "cherry", "banana", "kiwi"],
     init: function() {
+        const slotButton = document.querySelector(".button")
+        slotButton.addEventListener("click", this.insertImg)
 
     },
+
+    insertImg: function() {
+        let img = slotImg;
+        console.log(img)
+        const randomElement = img[Math.floor(Math.random() * img.length)];
+        let slots = document.querySelectorAll(".slot");
+        for (let slot of slots){
+            slot.dataset.status = img[Math.floor(Math.random() * img.length)];
+        }
+    }
 
 }
 

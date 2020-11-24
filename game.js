514c1da4a966cd78f42bd249142423f38b7206c0
Gombb <primegombb@protonmail.com>
2020-11-24 11:30:17 +0100

    refactor: restructure html and incorporate reel wheel feature

--- game.js
+++ game.js
@@ -9,19 +9,33 @@ const game = {
     insertImg: function() {
         let img = slotImg;
         let slots = document.querySelectorAll(".slot");
-        let anim = setInterval(setImage, 100, slots)
-        setTimeout(function () {
-            clearInterval(anim)
-        }, 3000)
-        function setImage(slots) {
-            for (let slot of slots) {
-                slot.dataset.status = img[Math.floor(Math.random() * img.length)];
-            }
-        }
-
+        let newPos = 0;
+        setInterval(function (){
+        newPos = newPos + 30;
+            slots[0].style.backgroundPositionY = newPos + "px";
+            slots[1].style.backgroundPositionY = newPos + "px";
+            slots[2].style.backgroundPositionY = newPos + "px";
+            slots[3].style.backgroundPositionY = newPos + "px";
+            slots[4].style.backgroundPositionY = newPos + "px";
+            slots[5].style.backgroundPositionY = newPos + "px";
+            slots[6].style.backgroundPositionY = newPos + "px";
+            slots[7].style.backgroundPositionY = newPos + "px";
+            slots[8].style.backgroundPositionY = newPos + "px";
+        }, 100);
     }
+
 }
 
 
 
 game.init();
+
+
+
+        /*let anim = setInterval(setImage, 100, slots)
+        setTimeout(function () {
+            clearInterval(anim)
+        }, 3000)
+        function setImage(slots) {
+                //slot.dataset.status = img[Math.floor(Math.random() * img.length)];
+        }*/

953e157efc60a822664de4d2e5d6a667e9700244
TerryD4vis <baphomet14@gmail.com>
2020-11-24 01:57:26 +0100

    feat: make slots change picture

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
 initGame();
 
 function initGame() {
+    let slots = document.getElementsByClassName("slot");
+    let newPos = 0;
+    setInterval(function (){
+        newPos = newPos + 30;
+        slots[0].style.backgroundPositionY = newPos + "px";
+        slots[1].style.backgroundPositionY = newPos + "px";
+        slots[2].style.backgroundPositionY = newPos + "px";
+    }, 100);
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+
 
 }
+

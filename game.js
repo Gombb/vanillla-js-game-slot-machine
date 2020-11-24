b9c5c2cd40fc47c78e9075372a5949328ba884be
TerryD4vis <baphomet14@gmail.com>
2020-11-24 16:00:12 +0100

    feat: add blur effect to the reel in motion

--- game.js
+++ game.js
@@ -3,15 +3,18 @@ window.onload = function() {
         init: function () {
             let slots = document.getElementsByClassName("slot");
             let slot0 = slots[0];
+            let slot1 = slots[1];
+            let slot2 = slots[2];
             let startButton = document.getElementById("button-start");
             let stopButton = document.getElementById("button-stop");
             var roller;
 
             stopButton.addEventListener("click", stopReels);
-            startButton.addEventListener("click", addInterval)
+            startButton.addEventListener("click", startRolling);
 
 
-            function addInterval() {
+            function startRolling() {
+                slot0.classList.add("motion");
                 roller = setInterval(rollReels, 100);
             }
 
@@ -22,6 +25,7 @@ window.onload = function() {
             }
 
             function stopReels() {
+                slot0.classList.remove("motion");
                 clearInterval(roller);
             }
         },

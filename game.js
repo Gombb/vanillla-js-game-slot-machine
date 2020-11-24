ff6e73a8c35e7059c37d1c978da25acc2d773c96
Gombb <primegombb@protonmail.com>
2020-11-24 14:15:21 +0100

    feat: pick random px from images

--- game.js
+++ game.js
@@ -1,26 +1,44 @@
 const game = {
-    const: slotImg = ["apple", "cherry", "banana", "kiwi"],
+    cons: slotImgPxs = {
+        orange: 0,
+        seven: 80,
+        bar: 160,
+        apricot: 240,
+        banana: 320,
+        cherry: 400
+
+    },
     init: function() {
         const slotButton = document.querySelector(".start-button")
-        slotButton.addEventListener("click", this.insertImg)
+        slotButton.addEventListener("click", this.setImage)
 
     },
 
-    insertImg: function() {
-        let img = slotImg;
+    setImage: function() {
+        let slots = document.querySelectorAll(".slot")
+        let randomProperty = function (obj) {
+            let keys = Object.keys(obj);
+            return obj[keys[ keys.length * Math.random() << 0]];
+        };
+        let newPos0 = randomProperty(slotImgPxs)
+        let newPos1 = randomProperty(slotImgPxs)
+        let newPos2 = randomProperty(slotImgPxs)
+        slots[0].style.backgroundPositionY = newPos0 + "px";
+        slots[1].style.backgroundPositionY = newPos1 + "px";
+        slots[2].style.backgroundPositionY = newPos2 + "px";
+
+    },
+
+
+
+    wheelInMotion: function() {
         let slots = document.querySelectorAll(".slot");
         let newPos = 0;
         setInterval(function (){
-        newPos = newPos + 30;
+            newPos = newPos + 30;
             slots[0].style.backgroundPositionY = newPos + "px";
             slots[1].style.backgroundPositionY = newPos + "px";
             slots[2].style.backgroundPositionY = newPos + "px";
-            slots[3].style.backgroundPositionY = newPos + "px";
-            slots[4].style.backgroundPositionY = newPos + "px";
-            slots[5].style.backgroundPositionY = newPos + "px";
-            slots[6].style.backgroundPositionY = newPos + "px";
-            slots[7].style.backgroundPositionY = newPos + "px";
-            slots[8].style.backgroundPositionY = newPos + "px";
         }, 100);
     }
 

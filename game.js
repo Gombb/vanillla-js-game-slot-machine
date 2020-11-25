5d3226f5aa460664a1182491fb594cdc2ed52914
Gombb <primegombb@protonmail.com>
2020-11-25 13:54:08 +0100

    feat: add basic feature

--- game.js
+++ game.js
@@ -1,7 +1,27 @@
-initGame();
+const game = {
+    const: slotImg = ["psql", "java", "javascript", "python", "c++"],
+    init: function() {
+        const slotButton = document.querySelector(".start-button")
+        slotButton.addEventListener("click", this.insertImg)
 
-function initGame() {
+    },
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+    insertImg: function() {
+        let img = slotImg;
+        let slots = document.querySelectorAll(".slot");
+        let anim = setInterval(setImage, 100, slots)
+        setTimeout(function () {
+            clearInterval(anim)
+        }, 3000)
+        function setImage(slots) {
+            for (let slot of slots) {
+                slot.dataset.status = img[Math.floor(Math.random() * img.length)];
+            }
+        }
 
+    }
 }
+
+
+
+game.init();

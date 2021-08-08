b7e9b1cc852f0b2c2bea142d2491f5fbac670918
gombb <primegombb@protonmail.com>
2021-08-08 13:49:51 +0200

    fix: init script

--- init_monitoring.sh
+++ init_monitoring.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 kubectl create namespace monitoring
+
+cd ./kub-prometheus
+kubectl apply -f ./clusterRole.yaml -f ./config-map.yaml -f ./prometheus-deployment.yaml -f ./prometheus-service.yaml
+cd ../kub-grafana

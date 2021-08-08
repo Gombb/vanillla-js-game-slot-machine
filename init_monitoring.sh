890e8b64afd67c06a8630be1cccd11224a303886
gombb <primegombb@protonmail.com>
2021-08-08 14:00:22 +0200

    feat: update init script with grafana service

--- init_monitoring.sh
+++ init_monitoring.sh
@@ -4,4 +4,6 @@ kubectl create namespace monitoring
 
 cd ./kub-prometheus
 kubectl apply -f ./clusterRole.yaml -f ./config-map.yaml -f ./prometheus-deployment.yaml -f ./prometheus-service.yaml
+
 cd ../kub-grafana
+kubectl apply -f ./grafana-datasource-config.yaml -f ./deployment.yaml -f ./service.yaml

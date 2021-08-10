d98217538b0258727c04d3392bd3a54d794319b9
gombb <primegombb@protonmail.com>
2021-08-10 22:15:11 +0200

    refactor: remove Dockerbuild line from init sh script

--- init_cluster.sh
+++ init_cluster.sh
@@ -9,6 +9,5 @@ cd ../kub-grafana
 kubectl apply -f ./grafana-datasource-config.yaml -f ./deployment.yaml -f ./service.yaml
 
 cd ../
-sudo docker build -f kub-nginx/Dockerfile . -t gombb:js-app
 kubectl apply -f kub-nginx/app-deployment.yaml
 kubectl apply -f kub-nginx/app-service.yaml
